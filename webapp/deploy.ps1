d90259bb1a10325fd061b078bc5c2441ac02a9b2
Diego Torres <diegotorres0303@gmail.com>
2022-07-14 14:02:37 -0500

    adding scripts

--- webapp/deploy.ps1
+++ webapp/deploy.ps1
@@ -1,6 +1,12 @@
+$WebappConfig=Get-Content -Raw -Path '..\webapp.json' | ConvertFrom-Json -Depth 4
+$bucketName="s3://" + $WebappConfig.webapp.webappBucketName
+$distributionId=$WebappConfig.webapp.distributionId
 
 #  [ ] 1.1.2: add command to update web assets in S3 [docs](https://docs.aws.amazon.com/cli/latest/reference/s3/index.html)
-aws s3 cp . s3://webapp-webappartifact89ba74f3-f3lsmkokb0hw --recursive
+
+Write-Host 'Uploadin assets to' + $bucketName
+aws s3 cp .  $bucketName --recursive
+
 
 #  [ ] 1.2.2: add command to invalidate cloudfront distribution [docs](https://docs.aws.amazon.com/cli/latest/reference/cloudfront/create-invalidation.html)
-aws cloudfront create-invalidation --distribution-id E3ON6CC6RA68P6 --paths '/*'
+aws cloudfront create-invalidation --distribution-id $distributionId --paths '/*'
