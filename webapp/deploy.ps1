da2e1928405b5b526a370923ad0ad9c9e653d381
Diego Torres <diegotorres0303@gmail.com>
2022-07-14 12:21:24 -0500

    showing orders in ui

--- webapp/deploy.ps1
+++ webapp/deploy.ps1
@@ -1,6 +1,6 @@
 
 #  [ ] 1.1.2: add command to update web assets in S3 [docs](https://docs.aws.amazon.com/cli/latest/reference/s3/index.html)
-aws s3 cp . s3://webapp-py-webappbucketa26a0839-1pfiy94uokgrg --recursive
+aws s3 cp . s3://webapp-webappartifact89ba74f3-f3lsmkokb0hw --recursive
 
 #  [ ] 1.2.2: add command to invalidate cloudfront distribution [docs](https://docs.aws.amazon.com/cli/latest/reference/cloudfront/create-invalidation.html)
-aws cloudfront create-invalidation --distribution-id E1U4BVJGRRZ4S2 --paths '/*'
+aws cloudfront create-invalidation --distribution-id E3ON6CC6RA68P6 --paths '/*'
